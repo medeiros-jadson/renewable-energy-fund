ff8b3c6d3fc48d958d2a7f45a41a353ac7aca834
Jadson Medeiros <jadson.asmedeiros@gmail.com>
2023-05-20 10:37:32 -0300

    chore(code-style): setup eslint, prettier and editorconfig

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
